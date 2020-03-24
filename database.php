98d2b85638bf0698e38478c1d4393be95a731f7b
pdjoon <58138560+pdjoon@users.noreply.github.com>
2020-03-24 11:02:57 +0700

    Update database.php

--- database.php
+++ database.php
@@ -2,8 +2,8 @@
 
 $dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbusername = 'root';
-$dbpassword = 'ubaya';
-$dbname = 'ubayadb';
+$dbpassword = 'juan';
+$dbname = 'juandb';
 
 $c = new mysqli($dbhost, $dbusername, $dbpassword, $dbname);
 if ($c->connect_error) {
